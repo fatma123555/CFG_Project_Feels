b11dba8bf2c065ec5973bead3f431dddc129a4ee
Fatma El <felasouad555@yahoo.com>
2021-08-11 13:12:38 +0100

    extracted code to create specialised get_playlist response function, and then a function to extract the elements of the resopnse into a data pack dictionary with URL, IMG and CODE

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -13,6 +13,22 @@ load_dotenv('.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
+def get_playlist_response(final_mood):
+    playlist_finder = SpotifyPlaylist()
+    playlist_finder.find_playlist(final_mood)
+    playlist_finder.get_playlist_url()
+    playlist_finder.get_playlist_image()
+    response = playlist_finder.combining_all()
+    return response
+
+
+def get_playlist_data(response):
+    playlist_data = {'URL': response[1]['spotify'],
+                     'IMG-URL': response[2][0]['url'],
+                     'CODE': response[1]['spotify'].split('/')[-1]
+                     }
+    return playlist_data
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = SECRET_KEY
@@ -36,16 +52,8 @@ def create_app():
         form.mood_2.choices = form.all_moods[mood]
         if form.validate_on_submit():
             final_mood = form.mood_2.data
-            playlist_finder = SpotifyPlaylist()
-            playlist_finder.find_playlist(final_mood)
-            playlist_finder.get_playlist_url()
-            playlist_finder.get_playlist_image()
-            response = playlist_finder.combining_all()
-            playlist_code = response[1]['spotify'].split('/')[-1]
-            playlist_data = {'URL': response[1]['spotify'],
-                             'IMG-URL': response[2][0]['url'],
-                             'CODE': playlist_code
-                             }
+            response = get_playlist_data(final_mood)
+            playlist_data = get_playlist_data(response)
             session['data'] = playlist_data
             return redirect(url_for('save_rating', final_mood=final_mood))
         return render_template("quiz_second_question.html", form=form, mood=mood)
