17ff6ccc701a11411d95562a4c18651c0b02ba94
Fatma El <felasouad555@yahoo.com>
2021-08-17 16:07:18 +0100

    cleaned up the feels_database python file

--- src/website/database/feels_database.py
+++ src/website/database/feels_database.py
@@ -7,29 +7,11 @@ try:
     moods_path = data_folder / "moods.csv"
 except Exception as e:
     print("Some modules are missing {}".format(e))
-# print(moods_path)
-# file = pd.read_csv(moods_path)
 
-# # Read only playlist name and score
-# # import pandas as pd
-# file = pd.read_csv('playlist_score.csv', skipinitialspace=True, usecols=['PLAYLIST NAME', 'SCORE'])
-# print(file)
-
-# def read_csv(path):
-#     pd_read =
-
-# Read both playlist mood and score (JOIN function)
-# import pandas as pd
-# pmood = pd.read_csv('playlist_mood.csv')
-# pscore = pd.read_csv('playlist_score.csv')
-# output1 = pd.merge(pmood, pscore,
-#                    on=['playlist_name','URL'],
-#                    how='inner')
-# print(output1)
 """ 
-method that will add a new mood as a header and fill the values of that 
-column with a list of sub_moods, if it isn't long enough, then the values
-will be NaN values
+    method that will add a new mood as a header and fill the values of that 
+    column with a list of sub_moods, if it isn't long enough, then the values
+    will be NaN values
 """
 def add_new_mood(main_mood, sub_moods):
     df = pd.read_csv(moods_path)
@@ -37,17 +19,16 @@ def add_new_mood(main_mood, sub_moods):
     df.to_csv(moods_path, index=False)
 
 """ 
-method that will get and return the headers and return it as a list
+    method that will get and return the headers and return it as a list
 """
 def get_main_moods():
     with open(moods_path, 'r') as f:
         pd_reader = pd.read_csv(f)
         return list(pd_reader.head())
 
-# print(get_main_moods())
 
 """ 
-method that will get and return the values in a specific column and return it as a list
+    method that will get and return the values in a specific column and return it as a list
 """
 def get_sub_moods(main_mood):
     with open(moods_path, 'r') as f:
@@ -55,34 +36,10 @@ def get_sub_moods(main_mood):
     results = [x for x in pd_reader[main_mood].values if str(x) != 'nan']
     return results
 
-# print(get_sub_moods("happy"))
-# print(file)
-# add_new_mood("Angry", ["livid", "furious", "resentful"])
-# print("*" * 30)
-# print("*" * 30)
-# file = pd.read_csv(moods_path)
-# print(file)
-# print("*" * 30)
-# print("*" * 30)
-# print("get main moods:", get_main_moods())
-# print("Get the sub mood for Angry:",get_sub_moods("Angry"))
-
-# # function to add new descriptive words to existing main moods
-# def add_mood(happy, sad, irritable, loving):
-#     from csv import writer
-#     List=[happy, sad, irritable, loving]
-#     with open(moods_path, 'a') as f_object:
-#         writer_object = writer(f_object)
-#         writer_object.writerow(List)
-#         f_object.close()
-
-# add_mood('ecstatic', 'miserable', 'terrible', 'sweet')
-
 data_folder = Path("./website/database/")
 playlist_score_path = data_folder / "playlist_score.csv"
 data_folder = Path("./website/database/")
 playlist_mood_path = data_folder / "playlist_mood.csv"
-# print(playlist_mood_path)
 
 # function to add new playlist not already in the CSV file to save the playlist and its moods
 def add_playlist(name, URL, main_mood, sub_mood):
@@ -94,11 +51,6 @@ def add_playlist(name, URL, main_mood, sub_mood):
                    "sub_mood": sub_mood}
         df = df.append(new_row, ignore_index=True)
     df.to_csv(playlist_mood_path, index=False)
-
-
-#add_playlist('nameA', 'URLA', 'loving', 'heartbroken')
-
-# print(playlist_score_path)
 
 # function to update the score - adds new score to existing score
 def update_score(name, URL, new_score):
@@ -113,16 +65,6 @@ def update_score(name, URL, new_score):
                    "score": new_score}
         df = df.append(new_row, ignore_index=True)
     df.to_csv(playlist_score_path, index=False)
-
-
-# # Return all playlists based on a specific sub-mood
-# def all_by_mood(mood):
-#     import pandas as pd
-#     file=pd.read_csv(r"playlist_mood.csv")
-#     file_mood = file[file['sub_mood'] == mood]
-#     print(file_mood)
-
-# all_by_mood('depressed')
 
 #Return top 3 highest scoring playlist (any mood)
 def top_scores(sub_mood):
@@ -158,33 +100,7 @@ def get_top_scores_dict(sub_mood):
         3: [third['playlist_name'], third['URL'], third['main_mood'], third['sub_mood'], third['score']]
     }
 
-    print("FIRST:\n", scores_dict[1])
-    print("SECOND:\n", scores_dict[2])
-    print("THIRD:\n", scores_dict[3])
-
     return scores_dict
 
-# # Return top 3 playlists by score based on mood
-# def top_by_mood(mood):
-#     import pandas as pd
-#     pmood = pd.read_csv('playlist_mood.csv')
-#     pscore = pd.read_csv('playlist_score.csv')
-#     data = pd.merge(pmood, pscore,
-#                     on=['playlist_name','URL'],
-#                     how='inner')
-#     data.sort_values("main_mood", inplace = True)
-#     filter1 = data["main_mood"]==mood
-#     data.where(filter1, inplace = True)
-#     data.sort_values(["score"],
-#                     axis=0,
-#                     ascending=[False],
-#                     inplace=True)
-#     data = data.dropna()
-#     print(data[:3])
-
-# top_by_mood('happy')
-
-
-# # Function to add new main emotion
 
 
